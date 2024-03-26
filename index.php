99b7d9fc5409fd17ba9ca3dee55938fd5498d71b
python533 <143389231+python533@users.noreply.github.com>
2024-03-26 17:16:34 +0300

    Asallık Teoremi
    Js Asallık

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<script type="text/javascript">
+var sayı=prompt("Bir Sayı Girin:");
+var bolmax=math.sqrt(sayı);
+
+for (var i = 0; i < bolmax; i++) {
+if (i % bolmax == 0) {
+console.log("Sayılar Asal Değildir!")
+}else {
+console.log("Sayı Asaldır");
+}
+}
+</script>
+</body>
+</html>
